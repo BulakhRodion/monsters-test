5f030a3ed33b7bd70337f60431534265095692fd
Rodion Bulakh <bulakhrodion1@gmail.com>
2021-03-12 21:18:40 +0200

    Test app created

--- src/App.js
+++ src/App.js
@@ -21,7 +21,7 @@ class App extends Component {
 
   handleChange = (e) => {
     this.setState({ searchField: e.target.value });
-  }
+  };
 
   render() {
     const { monsters, searchField } = this.state;
@@ -30,6 +30,7 @@ class App extends Component {
     );
     return (
       <div className="App">
+        <h1>Monsters Rolodex</h1>
         <SearchBox
           placeholder="search monsters"
           handleChange={this.handleChange}
